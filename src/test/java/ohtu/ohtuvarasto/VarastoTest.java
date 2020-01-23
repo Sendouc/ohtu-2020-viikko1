3f9906b3644fce19b32f44da8cd0557bf1963425
Sendou <sendou77@gmail.com>
2020-01-23 16:12:48 +0200

    varasto testikattavuus 100

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -65,4 +65,56 @@ public class VarastoTest {
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
+    @Test
+    public void liikaaOttaminenToimii() {
+        varasto.otaVarastosta(15);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void toStringSisältääOikeanSaldon() {
+        assertTrue(varasto.toString().indexOf("10") != -1);
+    }
+
+    @Test
+    public void negaativisilleTilavuudellaSaadaanNolla() {
+        Varasto v2 = new Varasto(-1);
+        assertEquals(0, v2.getSaldo(), vertailuTarkkuus);
+
+        // Varasto.java luokassa on ilmeisesti bugi mutta ymmärsin että siihen
+        // ei tule koskea joten testi tässä muodossa
+        Varasto v3 = new Varasto(-1, 5);
+        assertEquals(-1, v3.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void varastoVoidaanLuodaAlkusaldolla() {
+        Varasto v2 = new Varasto(10, 5);
+        assertEquals(v2.getSaldo(), 5, vertailuTarkkuus);
+    }
+
+    @Test
+    public void negaativisilleAlkusaldollaTuleeNolla() {
+        Varasto v2 = new Varasto(10, -1);
+        assertEquals(v2.getSaldo(), 0, vertailuTarkkuus);
+    }
+
+    @Test
+    public void varastoonEiVoiLisataNegatiivisesti() {
+        varasto.lisaaVarastoon(-1);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void liikaaLisatessaMeneeTayteen() {
+        varasto.lisaaVarastoon(15);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void negatiivisestiEiVoiOttaa() {
+        varasto.lisaaVarastoon(5);
+        varasto.otaVarastosta(-1);
+        assertEquals(5, varasto.getSaldo(), vertailuTarkkuus);
+    }
 }
